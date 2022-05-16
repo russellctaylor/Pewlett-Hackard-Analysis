72f7ad5b59eae4d62b811d61ca5938569f73d05d
Russell Taylor <rctaylorllc@gmail.com>
2022-05-16 11:47:37 -0600

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -72,9 +72,7 @@ CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	to_date DATE NOT NULL
 );
 
 	FROM titles
